e5fa7f8c6629ae8cbee30cc1f7a135c3d8e9e16b
caitlinmiriam <caitlinmiriam@gmail.com>
2026-03-26 10:38:03 +0530

    Update FrequencyOfCharacters.java

--- Day7/FrequencyOfCharacters.java
+++ Day7/FrequencyOfCharacters.java
@@ -1,1 +1,16 @@
-
+public class FrequencyOfCharacters
+{
+	public static void main(String[] args) {
+		String s="Hi Guys Hello";
+		int [] freq=new int[256];
+		for(int i=0;i<s.length();i++){
+		    char ch=Character.toLowerCase(s.charAt(i));
+		    freq[ch]++;
+		}
+		for(int i=0;i<256;i++){
+		    if(freq[i]!=0){
+		        System.out.println((char)(i)+" "+freq[i]);
+		    }
+		}
+	}
+}
