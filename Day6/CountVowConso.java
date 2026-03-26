d35a9d3a23db1c344b205f314d18cf7fa537a556
caitlinmiriam <caitlinmiriam@gmail.com>
2026-03-26 08:57:32 +0530

    Create CountVowConso.java

---
+++ Day6/CountVowConso.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class CountVowConso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int v = 0, c = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    v++;
+                } else {
+                    c++;
+                }
+            }
+        }
+        System.out.println(v);
+        System.out.println(c);
+    }
+}
